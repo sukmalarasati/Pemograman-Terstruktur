d83a48dff697080c625f1460666d2f9d8a2e2ff4
sukmalarasati <89761105+sukmalarasati@users.noreply.github.com>
2021-12-09 22:40:37 +0700

    Delete Chapter 11 directory

--- Chapter 11/Python Project.py
+++
@@ -1,8 +0,0 @@
-from datetime import *
-def diffDate():
-    skrg = datetime.date(datetime.now())
-    x = date(2021, 12, 1)
-    delta = skrg - x
-    return delta.days
-
-print('Selisihnya adalah: ', diffDate(), 'hari')
